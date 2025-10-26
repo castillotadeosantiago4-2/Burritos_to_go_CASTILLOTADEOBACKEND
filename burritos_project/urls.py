45683211b80f81c86a7ad769142b62aa2d0048be
castillotadeosantiago4-2 <a24311003@uthermosillo.edu.mx>
2025-10-25 20:28:54 -0700

    Actualización de settings y urls

--- burritos_project/urls.py
+++ burritos_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def bienvenido(request):
+    return HttpResponse("¡Bienvenido a la API de Burritos To Go!")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('', bienvenido, name='bienvenido'),
 ]
